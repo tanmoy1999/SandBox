2fee8683a68ac85838491e148cf3167c810f88a3
tanny <tanmoymunshi1999@gmail.com>
2023-05-20 20:53:06 +0530

    test

--- script.js
+++ script.js
@@ -5,35 +5,23 @@ fetch('data.json')
         // Get the data container element
         const dataContainer = document.getElementById('data-container');
 
-        // Define the number of items per page
-        const itemsPerPage = 3;
+        // Get the search input and search button elements
+        const searchInput = document.getElementById('search-input');
+        const searchButton = document.getElementById('search-button');
 
-        // Calculate the total number of pages
-        const totalPages = Math.ceil(Object.keys(data).length / itemsPerPage);
-
-        // Function to display data for a specific page
-        function displayPage(page) {
+        // Function to display data for a specific stock
+        function displayStock(stock) {
             // Clear the data container
             dataContainer.innerHTML = '';
 
-            // Calculate the starting and ending indices of the data for the current page
-            const startIndex = (page - 1) * itemsPerPage;
-            const endIndex = startIndex + itemsPerPage;
-
-            // Get the keys of the stocks
-            const stockKeys = Object.keys(data);
-
-            // Iterate over the stocks for the current page
-            for (let i = startIndex; i < endIndex && i < stockKeys.length; i++) {
-                const stock = stockKeys[i];
+            // Check if the stock exists in the data
+            if (data.hasOwnProperty(stock)) {
+                const stockData = data[stock][0];
 
                 // Create a new heading element for the stock
                 const stockHeading = document.createElement('h2');
                 stockHeading.textContent = stock;
                 dataContainer.appendChild(stockHeading);
-
-                // Get the data for the current stock
-                const stockData = data[stock][0];
 
                 // Create an array to hold labels and data for the bar graph
                 const labels = [];
@@ -69,39 +57,21 @@ fetch('data.json')
                         }
                     }
                 });
+            } else {
+                // Display a message if the stock is not found
+                const errorMessage = document.createElement('p');
+                errorMessage.textContent = 'Stock not found';
+                dataContainer.appendChild(errorMessage);
             }
         }
 
-        // Function to create pagination links
-        function createPaginationLinks() {
-            // Create a pagination container
-            const paginationContainer = document.createElement('div');
-            paginationContainer.classList.add('pagination');
+        // Add event listener to the search button
+        searchButton.addEventListener('click', () => {
+            const stock = searchInput.value.trim();
+            displayStock(stock);
+        });
 
-            // Iterate over the total number of pages
-            for (let i = 1; i <= totalPages; i++) {
-                // Create a pagination link
-                const paginationLink = document.createElement('a');
-                paginationLink.href = '#';
-                paginationLink.textContent = i;
-
-                // Add an event listener to the pagination link
-                paginationLink.addEventListener('click', () => {
-                    displayPage(i);
-                });
-
-                // Append the pagination link to the container
-                paginationContainer.appendChild(paginationLink);
-            }
-
-            // Append the pagination container to the data container
-            dataContainer.appendChild(paginationContainer);
-        }
-
-        // Display the first page initially
-        displayPage(1);
-
-        // Create the pagination links
-        createPaginationLinks();
+        // Display the initial data
+        displayStock('20MICRONS');
     })
     .catch(error => console.error(error));
