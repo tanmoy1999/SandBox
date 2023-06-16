30be7be2536ee86f5cda2477ce9a129d52a67312
Tanmoy Munshi <tanmoymunshi1999@gmail.com>
2023-06-16 23:57:46 +0530

    inter

---
+++ inter.py
@@ -0,0 +1,16 @@
+a = {}
+
+lst = [1,2,4,4,6,2,2,3,5]
+str1 = "My Name is Tanmoy. And he is great."
+
+lst = str1.split(' ')
+
+for i in lst:
+    if i in a:
+        a[i] = a[i] + 1
+    else:
+        a[i] = 1
+    
+print(sorted(a))
+
+print(str1[::])
